c6236b7216d1f43f290f46db9503592017ed1ea5
Ricartho Franck <ricartho.franck@esih.edu>
2019-07-29 20:34:58 +0000

    save3

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,13 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    joins(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    joins(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+  end
+
+ 
 end
